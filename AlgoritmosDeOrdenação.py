a73d0ebd3df13b20c2d712b29a716bfe8fa1a19c
Mateus Fonseca <mateusfonsecafs@gmail.com>
2024-04-20 18:20:43 -0300

    AlgoritmosDeOrdenação

---
+++ AlgoritmosDeOrdenação.py
@@ -0,0 +1,67 @@
+import random
+import time
+
+#Criar Array aleatorio
+def gerar_array(tamanho_array):
+    array = [random.randint(0, 5 * tamanho_array) for _ in range(tamanho_array)]
+    return array
+
+
+#Bubble Sort
+def bubble_sort(ar):
+    n_elementos = len(ar)
+    trocou = True
+
+    while(trocou):
+        trocou = False
+        for i in range(n_elementos - 1):
+            if ar[i] > ar[i+1]:
+                ar[i], ar[i+1] = ar[i+1], ar[i]
+                trocou = True
+        n_elementos -= 1
+
+    return ar[:10]        
+
+
+#Selection Sort
+def selection_sort(ar):
+    n_elementos = len(ar)
+    for i in range(n_elementos - 1):
+        iMenor = i
+        for j in range(i+1, n_elementos):
+            if ar[j] < ar[iMenor]:
+                iMenor = j
+        ar[i], ar[iMenor] = ar[iMenor], ar[i]
+
+    return ar[:10]    
+
+
+#Insertion Sort
+def insertion_sort(ar):
+    n_elementos = len(ar)
+    for i in range(1, n_elementos):
+        aux = ar[i]
+        j = i - 1
+        while j >= 0 and aux < ar[j]:
+            ar[j + 1] = ar[j]
+            j -= 1
+        ar[j + 1] = aux
+
+    return ar[:10]
+
+
+
+def contador_sort(metodosort):
+    inicio = time.time()
+    ar = gerar_array(15000)
+
+    print(metodosort(ar))
+
+    fim = time.time()
+    tempo_de_execucao = fim - inicio
+    print("Tempo de execução: {:.2f} segundos".format(tempo_de_execucao))
+
+
+contador_sort(bubble_sort)
+contador_sort(selection_sort)
+contador_sort(insertion_sort)
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Python-AlgoritmosDeOrdenacao-1
 Este projeto consiste em uma implementação em Python de três algoritmos de ordenação com Custo Temporal Quadrático: Bubble Sort, Selection Sort e Insertion Sort. O objetivo principal é demonstrar a aplicação desses algoritmos na ordenação de arrays e comparar seus desempenhos em termos de tempo de execução.
+
+## Algoritmos de Ordenação Implementados
+
+1. **Bubble Sort:** Um algoritmo simples que compara pares de elementos adjacentes e os troca se estiverem na ordem errada. Repete esse processo até que o array esteja ordenado.
+![Bubble Sort](img_README\BubbleSort.png)
+
+2. **Selection Sort:** Este algoritmo divide o array em duas partes: uma parte ordenada e outra desordenada. Ele encontra o menor elemento na parte desordenada e o move para o início da parte ordenada.
+![Selection Sort](img_README\SelectionSort.png)
+
+3. **Insertion Sort:** Este algoritmo percorre o array e, para cada elemento, o insere na posição correta na parte já ordenada do array, deslocando os elementos maiores conforme necessário.
+![Insertion Sort](img_README\InsertionSort.png)
